66bbe55ff98ea4484186216772c572558defbabc
Konrad Pakula <Konrad.Pakula@Live.umcs.edu.pl>
2019-06-21 01:57:43 +0200

    Added get message end point

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, json
 from flask import request
-
+from flask import jsonify
 from client import send_request_to_server, create_login_request, create_message_request, module_var, start
 
 app = Flask(__name__)
@@ -25,6 +25,7 @@ def login():
         mimetype='application/json'
     )
 
+
 @app.route('/sendmessage', methods=['POST'])
 def send_message():
 
@@ -33,6 +34,22 @@ def send_message():
 
     return app.response_class(
         response=json.dumps({'code': 'ok'}),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/messages', methods=['GET'])
+def get_all_available_message():
+    all_messages_response = []
+
+    if module_var.all_messages:
+        all_messages_response = json.dumps(module_var.all_messages)
+        print(all_messages_response)
+        module_var.all_messages.clear()
+
+    return app.response_class(
+        response=all_messages_response,
         status=200,
         mimetype='application/json'
     )
