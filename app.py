d84eb303e47b71745b07de64904c6af8818f69b9
albert <albert.kulinski@hotmail.com>
2019-06-21 13:28:37 +0200

    Cors error fixing WIP

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, json
 from flask import request
-from flask import jsonify
+from flask_cors import CORS, cross_origin
+
 from client import send_request_to_server, create_login_request, create_message_request, module_var, start
 
 app = Flask(__name__)
+CORS(app)
 start()
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/sendmessage', methods=['POST'])
+def send_message():
+    data = json.loads(request.data)
+    send_request_to_server(create_message_request(data['login'], data['message']))
+
+    return app.response_class(
+        response=json.dumps({'code': 'ok'}),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/login', methods=['POST'])
+@cross_origin()
 def login():
     body = request.data
     data = json.loads(body)
@@ -22,24 +37,11 @@ def login():
     return app.response_class(
         response=json.dumps(response),
         status=200,
-        mimetype='application/json'
+        mimetype='application/json',
     )
 
 
-@app.route('/sendmessage', methods=['POST'])
-def send_message():
-
-    data = json.loads(request.data)
-    send_request_to_server(create_message_request(data['login'],data['message']))
-
-    return app.response_class(
-        response=json.dumps({'code': 'ok'}),
-        status=200,
-        mimetype='application/json'
-    )
-
-
-@app.route('/messages', methods=['GET'])
+@app.route('/api/messages', methods=['GET'])
 def get_all_available_message():
     all_messages_response = []
 
@@ -54,9 +56,11 @@ def get_all_available_message():
         mimetype='application/json'
     )
 
-@app.route('/onlineusers', methods=['GET'])
+
+@app.route('/api/onlineusers', methods=['GET'])
 def get_online_users():
     return json.dumps({"online:": module_var.online_list})
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_evalex=False)
