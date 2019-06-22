55b4df38076d71e4a4b080850f2bd2ed8925be53
Tomasz <tomasz.j.gawel@gmail.com>
2019-06-22 19:40:49 +0200

    loogin in var flag fix

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import time
 import event_parser
 import event_types
 
-host = "localhost"
+host = "192.168.0.47"
 port = 8889
 
 module_var = sys.modules[__name__]
@@ -21,7 +21,7 @@ def get_data_from_server():
     read_size = 1024
     while True:
         data = server_socket.recv(1024)
-        print("Data?:" + data.decode())
+        #print("Data?:" + data.decode())
         full_length = event_parser.get_full_length(data.decode())
 
         while read_size < full_length:
@@ -29,7 +29,7 @@ def get_data_from_server():
             read_size += 1024
 
         received_response = data.decode()
-        #print("RECEIVED: " + received_response)
+        print("RECEIVED: " + received_response)
 
         if received_response:
             #print("Starting making events: ")
@@ -39,7 +39,7 @@ def get_data_from_server():
 def create_event_from_string(response_from_server):
     pr = event_parser.EventParser()
     event_from_server = pr.parse_string_to_event(response_from_server)
-
+    print(event_from_server)
     #print("Create_event_from_string_start")
 
     if event_from_server.event_type == event_types.LOGIN_RESPONSE:
@@ -58,7 +58,7 @@ def create_event_from_string(response_from_server):
         event_from_server_object = {'user': event_from_server.login,
                                     'messageText': event_from_server.message,
                                     'created': event_from_server.timestamp}
-        print(event_from_server_object)
+        #print(event_from_server_object)
         module_var.all_messages.append(event_from_server_object)
 
     elif event_from_server.event_type == event_types.ONLINE_REQUEST:
@@ -69,20 +69,20 @@ def create_event_from_string(response_from_server):
 def create_login_request(username):
     login_request = event_types.LoginRequest(username)
     login_request_string = login_request.convert_to_string()
-    print(login_request_string)
+    #print(login_request_string)
     return login_request_string
 
 
 def create_logout_request(username, is_logged_out):
     logout_request = event_types.LogoutRequest(username, is_logged_out)
     logout_request_string = logout_request.convert_to_string()
-    print(logout_request_string)
-    # module_var.isLogged = False
+    #print(logout_request_string)
+    module_var.isLogged = False
     return logout_request_string
 
 
 def send_request_to_server(request_string):
-    #print('Send: %r' % request_string)
+    print('Send: %r' % request_string)
     server_socket.send(request_string.encode())
     time.sleep(0.1)
 
@@ -90,7 +90,7 @@ def send_request_to_server(request_string):
 def create_message_request(username, message):
     message_request = event_types.MessageRequest(username, message)
     message_request_string = message_request.convert_to_string()
-    print(message_request_string)
+    #print(message_request_string)
     return message_request_string
 
 
