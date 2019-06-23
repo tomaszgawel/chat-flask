28bffdd546cb84b521a970648994656abad48642
Konrad Pakula <Konrad.Pakula@Live.umcs.edu.pl>
2019-06-23 10:22:43 +0200

    Added logs to chat flask, disable showing http methods

--- client.py
+++ client.py
@@ -3,11 +3,17 @@ import ssl
 import sys
 import threading
 import time
-
+import logging
 import event_parser
 import event_types
 
-host = "192.168.0.47"
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+host = "192.168.0.73"
 port = 8889
 
 module_var = sys.modules[__name__]
@@ -21,7 +27,6 @@ def get_data_from_server():
     read_size = 1024
     while True:
         data = server_socket.recv(1024)
-        #print("Data?:" + data.decode())
         full_length = event_parser.get_full_length(data.decode())
 
         while read_size < full_length:
@@ -29,36 +34,30 @@ def get_data_from_server():
             read_size += 1024
 
         received_response = data.decode()
-        print("RECEIVED: " + received_response)
+        logging.info("Received: " + received_response)
 
         if received_response:
-            #print("Starting making events: ")
             create_event_from_string(received_response)
 
 
 def create_event_from_string(response_from_server):
     pr = event_parser.EventParser()
     event_from_server = pr.parse_string_to_event(response_from_server)
-    print(event_from_server)
-    #print("Create_event_from_string_start")
 
     if event_from_server.event_type == event_types.LOGIN_RESPONSE:
-        #print("\nLogin response: ")
 
         if event_from_server.code == event_types.CODE_ACCEPT:
-            print("ACCEPTED")
+            logging.info("LOGIN ACCEPTED")
             module_var.isLogged = True
 
         # When username exists
         elif event_from_server.code == event_types.CODE_REJECT:
-            print("REJECT LOGIN")
+            logging.info("LOGIN REJECT")
 
     elif event_from_server.event_type == event_types.MESSAGE_REQUEST:
-        # print("\nMessage\n{}: {}".format(event_from_server.login, event_from_server.message))
         event_from_server_object = {'user': event_from_server.login,
                                     'messageText': event_from_server.message,
                                     'created': event_from_server.timestamp}
-        #print(event_from_server_object)
         module_var.all_messages.append(event_from_server_object)
 
     elif event_from_server.event_type == event_types.ONLINE_REQUEST:
@@ -69,20 +68,18 @@ def create_event_from_string(response_from_server):
 def create_login_request(username):
     login_request = event_types.LoginRequest(username)
     login_request_string = login_request.convert_to_string()
-    #print(login_request_string)
     return login_request_string
 
 
 def create_logout_request(username, is_logged_out):
     logout_request = event_types.LogoutRequest(username, is_logged_out)
     logout_request_string = logout_request.convert_to_string()
-    #print(logout_request_string)
     module_var.isLogged = False
     return logout_request_string
 
 
 def send_request_to_server(request_string):
-    print('Send: %r' % request_string)
+    logging.info('Sent to server: %r' % request_string)
     server_socket.send(request_string.encode())
     time.sleep(0.1)
 
@@ -90,7 +87,6 @@ def send_request_to_server(request_string):
 def create_message_request(username, message):
     message_request = event_types.MessageRequest(username, message)
     message_request_string = message_request.convert_to_string()
-    #print(message_request_string)
     return message_request_string
 
 
